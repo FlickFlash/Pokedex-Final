4bc00444547b76accc623b599eecb308ac68fdf7
FlickFlash <joseroberto_fsa@live.com>
2022-08-01 22:08:57 -0300

    Search input now loads after spinner; Pokemons now can be found (scrolledTo) by inserting their number; Some mistakes can be found in this version and will be corrected soon.

--- pokedexFinal/app.js
+++ pokedexFinal/app.js
@@ -8,7 +8,7 @@ const generateHTML = pokemons => pokemons.reduce((accumulator, { types, id, spec
         const elementTypes = types.map(typeInfo => typeInfo.type.name)
         if (id <= 898) {
             accumulator += `
-            <li class="card ${elementTypes[0]}">
+            <li class="card ${elementTypes[0]}" id="pokemon${id}">
             <img class="card-image" alt="${species.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" />
                 <h2 class="card-title">${id}. ${species.name}
                 <p class="card-subtitle">${elementTypes.join(' | ')}</p>
@@ -16,7 +16,7 @@ const generateHTML = pokemons => pokemons.reduce((accumulator, { types, id, spec
             return accumulator
         } else {
             accumulator += `
-            <li class="card ${elementTypes[0]}">
+            <li class="card ${elementTypes[0]}" id="pokemon${id}">
             <img class="card-image" alt="${species.name}" src="./img/imgpokemon${id}.png" />
                 <h2 class="card-title">${id}. ${species.name}
                 <p class="card-subtitle">${elementTypes.join(' | ')}</p>
@@ -32,16 +32,38 @@ const insertPokemonsIntoPage = pokemons => {
 }
 
 const spinner = document.getElementById("loadingSpinner")
+
 const removeSpinner = () => setTimeout(() => {
     spinner.remove()
 }, 0);
 
+const searchingPokemonsBlock = document.getElementById("searchBlock")
+let searchingPokemonsInput = document.createElement("input")
+searchingPokemonsInput.type = "text"
+searchingPokemonsInput.placeholder = "Nome ou número do Pokémon..."
+searchingPokemonsInput.id = "searchPokemon"
+
+
+const searchingPokemonsButton = document.createElement("button")
+searchingPokemonsButton.onclick = scrollToPokemon
+searchingPokemonsButton.innerText = "BUSCAR"
+
+function appendSearch() {
+    searchingPokemonsBlock.appendChild(searchingPokemonsInput)
+    searchingPokemonsBlock.appendChild(searchingPokemonsButton)
+}
+
 const pokemonPromises = generatePokemonPromises()
 
-
-
+function scrollToPokemon(id) {
+    // alert("clicou")
+    // console.log("clicou")
+    searchingPokemonsInput = searchingPokemonsInput.value.toLowerCase()
+    location.href = "#pokemon" + searchingPokemonsInput
+}
 
 Promise.all(pokemonPromises)
     .then(generateHTML)
     .then(insertPokemonsIntoPage)
     .then(removeSpinner)
+    .then(appendSearch)
